20d4ef6a1ebe48101c6efb5383c5b4c823ccb972
Jl115 <joelbern006@gmail.com>
2023-10-23 10:37:06 +0200

    adding roles to CategoryController endpoints for security

--- src/main/java/com/example/uek295_backeend/category/CategoryController.java
+++ src/main/java/com/example/uek295_backeend/category/CategoryController.java
@@ -15,49 +15,37 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    // GET Mapping
-    @GetMapping({"/category/{id}"})
+    @GetMapping("/category/{id}")
     public CategoryDTO getCategoriesById(@PathVariable Long id) {
         return categoryService.getById(id);
     }
 
-    @GetMapping({"/category"})
+    @GetMapping("/categories")
     public List<CategoryDTO> getCategories() {
         return categoryService.getAll();
     }
 
-    // POST Mapping
     @PostMapping("/categories")
-    @PreAuthorize("hasRole('ADMIN')")  // Ensure that only admins can access this endpoint
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> createCategory(@RequestBody CategoryDTO categoryDTO) {
         categoryService.createCategory(categoryDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body("Category created successfully");
-    }
-    @GetMapping("/categories")
-    public ResponseEntity<List<Category>> getAllCategories() {
-        return ResponseEntity.ok(categoryService.getAllCategories());
     }
 
     @GetMapping("/categories/{categoryId}")
     public ResponseEntity<Category> getCategoryById(@PathVariable int categoryId) {
         return ResponseEntity.ok(categoryService.getCategoryById(categoryId));
     }
+
     @PutMapping("/categories/{categoryId}")
-    @PreAuthorize("hasRole('ADMIN')")  // Ensure that only admins can access this endpoint
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> updateCategory(@PathVariable int categoryId, @RequestBody CategoryDTO categoryDTO) {
         categoryService.updateCategory(categoryId, categoryDTO);
         return ResponseEntity.ok("Category updated successfully");
     }
 
-
-    // Delete Mapping
-    @DeleteMapping("/category/{id}")
-    public String deleteCategory(@PathVariable String id) {
-        categoryService.delete(Long.parseLong(id));
-        return "Category with id: " + id + " deleted successfully";
-    }
     @DeleteMapping("/categories/{categoryId}")
-    @PreAuthorize("hasRole('ADMIN')")  // Ensure that only admins can access this endpoint
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> deleteCategory(@PathVariable int categoryId) {
         categoryService.deleteCategory(categoryId);
         return ResponseEntity.ok("Category deleted successfully");
